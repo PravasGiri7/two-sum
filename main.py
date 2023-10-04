8f399d8985d1007f2ef1a2f97ae7b51f41ba22ea
PravasGiri7 <gpravas4@gmail.com>
2023-10-04 19:06:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
 from typing import List
 
 def twoSum(self,nums: List[int], target: int) -> List[int]:
-    index = {}  # Dictionary to store numbers and their indices
-    
+    index = {}  
     for i, num in enumerate(nums):
-        remaining = target - num  # Calculate the complement
-        
-        # Check if the remaining exists in the dictionary
+        remaining = target - num         
         if remaining in index:
-            return [index[remaining], i]  # Return the indices of the two numbers
-        
-        # If not found, add the current number to the dictionary
+            return [index[remaining], i]  
         index[num] = i
-    
-    return []  # If no such pair is found, return an empty list
+    return [] 
